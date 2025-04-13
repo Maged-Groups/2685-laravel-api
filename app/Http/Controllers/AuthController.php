fa39a94518f7622c09dcddb882cc9714ee0ace58
Maged <magedyaseengroups@gmail.com>
2025-04-13 21:45:49 +0200

    add auth:sanctum middleware, and add API Tokens

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use App\Http\Requests\LoginRequest;
+use App\Http\Resources\UserResource;
 use App\Models\User;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
@@ -13,6 +14,18 @@ class AuthController extends Controller
     {
 
         if (Auth::attempt($request->validated())) {
+
+            // $user = User::where('email', $request->email)->first();
+
+            /** @var App\Models\User $user  */
+            $user = Auth::user();
+
+            $token = $user->createToken('desktop_login')->plainTextToken;
+
+            $user->token = $token;
+
+            return UserResource::make($user);
+
             return 'user logged in successfully';
         } else {
             return 'Failed to login';
--- routes/api.php
+++ routes/api.php
@@ -2,16 +2,21 @@
 
 use App\Http\Controllers\AuthController;
 use App\Http\Controllers\PostController;
-
+use App\Http\Controllers\UserController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/user', function (Request $request) {
-    return $request->user();
-})->middleware('auth:sanctum');
+
+// Protected Routes
+Route::middleware('auth:sanctum')->group(function () {
+    Route::resource('posts', PostController::class);
+
+    Route::resource('users', UserController::class);
+});
 
 
-Route::resource('posts', PostController::class);
 
 
+
+// Unprotected Routes
 Route::post('auth/login', [AuthController::class, 'login']);
